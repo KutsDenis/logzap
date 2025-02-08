b26884dc4b079d5539ad30ae21278104ade6966f
Kuts Denis <den.is.kuts@icloud.com>
2025-02-08 14:02:34 +0500

    added logging methods

--- logzap.go
+++ logzap.go
@@ -32,3 +32,23 @@ func Sync() {
 		_ = Logger.Sync()
 	}
 }
+
+func Debug(msg string, fields ...zap.Field) {
+	Logger.Debug(msg, fields...)
+}
+
+func Info(msg string, fields ...zap.Field) {
+	Logger.Info(msg, fields...)
+}
+
+func Warn(msg string, fields ...zap.Field) {
+	Logger.Warn(msg, fields...)
+}
+
+func Error(msg string, fields ...zap.Field) {
+	Logger.Error(msg, fields...)
+}
+
+func Fatal(msg string, fields ...zap.Field) {
+	Logger.Fatal(msg, fields...)
+}
