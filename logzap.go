5f6ea5ac216d16cbb7b23162fd10d44b4130a39b
Denis Kuts <Den.is.Kuts@icloud.com>
2025-04-04 13:11:37 +0500

    refactor logger implementation to return zap.Logger directly and remove unnecessary struct

--- logzap.go
+++ logzap.go
@@ -7,13 +7,8 @@ import (
 
 const prodENV = "prod"
 
-// Logger - структура логгера.
-type Logger struct {
-	zapLogger *zap.Logger
-}
-
 // NewLogger создает и возвращает новый логгер.
-func NewLogger(env string) *Logger {
+func NewLogger(env string) *zap.Logger {
 	config := getConfig(env)
 
 	var options []zap.Option
@@ -26,7 +21,7 @@ func NewLogger(env string) *Logger {
 		panic("Failed to initialize logger: " + err.Error())
 	}
 
-	return &Logger{zapLogger: zapLogger}
+	return zapLogger
 }
 
 // getConfig возвращает конфигурацию логгера в зависимости от окружения.
@@ -42,44 +37,4 @@ func getConfig(env string) zap.Config {
 	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
 
 	return config
-}
-
-// Debug логирует сообщение с уровнем Debug.
-func (l *Logger) Debug(msg string, fields ...zap.Field) {
-	l.zapLogger.Debug(msg, fields...)
-}
-
-// Info логирует сообщение с уровнем Info.
-func (l *Logger) Info(msg string, fields ...zap.Field) {
-	l.zapLogger.Info(msg, fields...)
-}
-
-// Warn логирует сообщение с уровнем Warn.
-func (l *Logger) Warn(msg string, fields ...zap.Field) {
-	l.zapLogger.Warn(msg, fields...)
-}
-
-// Error логирует сообщение с уровнем Error.
-func (l *Logger) Error(msg string, fields ...zap.Field) {
-	l.zapLogger.Error(msg, fields...)
-}
-
-// DPanic логирует сообщение с уровнем DPanic.
-func (l *Logger) DPanic(msg string, fields ...zap.Field) {
-	l.zapLogger.DPanic(msg, fields...)
-}
-
-// Panic логирует сообщение с уровнем Panic.
-func (l *Logger) Panic(msg string, fields ...zap.Field) {
-	l.zapLogger.Panic(msg, fields...)
-}
-
-// Fatal логирует сообщение с уровнем Fatal.
-func (l *Logger) Fatal(msg string, fields ...zap.Field) {
-	l.zapLogger.Fatal(msg, fields...)
-}
-
-// Sync завершает все буферизованные записи логов.
-func (l *Logger) Sync() {
-	_ = l.zapLogger.Sync()
 }
